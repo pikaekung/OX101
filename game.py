55c2ad4a6dd7f4d68ec38f9c282e073eed62ba52
Anuk Hongwanna <pikaekung@gmail.com>
2018-03-31 17:23:20 +0700

    - Check game rule.

--- game.py
+++ game.py
@@ -3,9 +3,17 @@ import sys
 import itertools
 
 ####
-game_rule_win_set = [[1, 2, 3], [4, 5, 6], [7, 8, 9], #Horizontal Set
-					 [1, 4, 7], [2, 5, 8], [3, 6, 9], #Vertical Set
-					 [1, 5, 9], [3, 5, 7]] #Diagonal Set
+# game_rule_win_set = [[1, 2, 3], [4, 5, 6], [7, 8, 9], #Horizontal Set
+# 					 [1, 4, 7], [2, 5, 8], [3, 6, 9], #Vertical Set
+# 					 [1, 5, 9], [3, 5, 7]] #Diagonal Set
+game_rule_win_set = [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1], 
+					 [4, 5, 6], [4, 6, 5], [5, 4, 6], [5, 6, 4], [6, 4, 5], [6, 5, 4], 
+					 [7, 8, 9], [7, 9, 8], [8, 7, 9], [8, 9, 7], [9, 7, 8], [9, 8, 7], 
+					 [1, 4, 7], [1, 7, 4], [4, 1, 7], [4, 7, 1], [7, 1, 4], [7, 4, 1], 
+					 [2, 5, 8], [2, 8, 5], [5, 2, 8], [5, 8, 2], [8, 2, 5], [8, 5, 2], 
+					 [3, 6, 9], [3, 9, 6], [6, 3, 9], [6, 9, 3], [9, 3, 6], [9, 6, 3], 
+					 [1, 5, 9], [1, 9, 5], [5, 1, 9], [5, 9, 1], [9, 1, 5], [9, 5, 1], 
+					 [3, 5, 7], [3, 7, 5], [5, 3, 7], [5, 7, 3], [7, 3, 5], [7, 5, 3]]			
 
 # all_game_rule_win_set = [list(itertools.permutations(for i in game_rule_win_set))]
 o_player = []
@@ -14,8 +22,12 @@ x_player = []
 player_turn = itertools.cycle(range(2))
 board = []
 
-def check_game_rule():
-	return
+def check_game_rule(player_list):
+	if len(player_list) >= 3:
+		for i in itertools.combinations(player_list, 3):
+			if list(i) in game_rule_win_set:
+				return True
+	return False
 
 def check_piece_exist(squre_position):
 	if squre_position in board:
@@ -68,13 +80,17 @@ while len(board) < 9:
 		x_player.append(i)
 
 	board.append(i)
-	check_game_rule()
-
 	# Display Board
 	# [1, 2, 3]
 	# [4, 5, 6]
 	# [7, 8, 9]
 	################
-	print_board()	
+	print_board()
+	if check_game_rule(o_player): 
+		print "O Player win this game."
+		break
+	if check_game_rule(x_player):
+		print "X Player win this game."
+		break
 
 print("End Game.")
--- test.py
+++ test.py
@@ -1,8 +1,23 @@
-import itertools
+# import itertools
 
-game_rule_win_set = [[1, 2, 3], [4, 5, 6], [7, 8, 9], #Horizontal Set
-					 [1, 4, 7], [2, 5, 8], [3, 6, 9], #Vertical Set
-					 [1, 5, 9], [3, 5, 7]] #Diagonal Set
+# game_rule_win_set = [[1, 2, 3], [4, 5, 6], [7, 8, 9], #Horizontal Set
+# 					 [1, 4, 7], [2, 5, 8], [3, 6, 9], #Vertical Set
+# 					 [1, 5, 9], [3, 5, 7]] #Diagonal Set
 
-all_game_rule_win_set = [list(itertools.permutations(i)) for i in game_rule_win_set]
-print all_game_rule_win_set
+# all_game_rule_win_set = [list(itertools.permutations(i)) for i in game_rule_win_set]
+# print all_game_rule_win_set
+
+
+a = [3, 5, 7]
+game_rule_win_set = [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1], 
+					 [4, 5, 6], [4, 6, 5], [5, 4, 6], [5, 6, 4], [6, 4, 5], [6, 5, 4], 
+					 [7, 8, 9], [7, 9, 8], [8, 7, 9], [8, 9, 7], [9, 7, 8], [9, 8, 7], 
+					 [1, 4, 7], [1, 7, 4], [4, 1, 7], [4, 7, 1], [7, 1, 4], [7, 4, 1], 
+					 [2, 5, 8], [2, 8, 5], [5, 2, 8], [5, 8, 2], [8, 2, 5], [8, 5, 2], 
+					 [3, 6, 9], [3, 9, 6], [6, 3, 9], [6, 9, 3], [9, 3, 6], [9, 6, 3], 
+					 [1, 5, 9], [1, 9, 5], [5, 1, 9], [5, 9, 1], [9, 1, 5], [9, 5, 1], 
+					 [3, 5, 7], [3, 7, 5], [5, 3, 7], [5, 7, 3], [7, 3, 5], [7, 5, 3]]			
+
+print a
+print game_rule_win_set
+print a in game_rule_win_set					
